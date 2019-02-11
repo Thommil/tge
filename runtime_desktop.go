d277fc59c734cd04f6d666617d919d5c68b94228
thommil <thomthom.millet@gmail.com>
2019-02-11 18:46:03 +0100

    Test glfw

--- runtime_desktop.go
+++ runtime_desktop.go
@@ -4,9 +4,36 @@
 
 package tge
 
-import log "log"
+import (
+	log "log"
+	runtime "runtime"
 
-func backend_Instanciate(app App) error {
+	glfw "github.com/go-gl/glfw/v3.2/glfw"
+)
+
+func init() {
+	runtime.LockOSThread()
+}
+
+func doInstanciate(app App) error {
 	log.Println("backend_Instanciate()")
+	err := glfw.Init()
+	if err != nil {
+		return err
+	}
+	defer glfw.Terminate()
+
+	window, err := glfw.CreateWindow(640, 480, "Testing", nil, nil)
+	if err != nil {
+		return err
+	}
+
+	window.MakeContextCurrent()
+
+	for !window.ShouldClose() {
+		// Do OpenGL stuff.
+		window.SwapBuffers()
+		glfw.PollEvents()
+	}
 	return nil
 }
