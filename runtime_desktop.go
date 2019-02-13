769dc5d8f57a581c73ba9d357925e33a50e9f855
thommil <thomthom.millet@gmail.com>
2019-02-13 09:35:39 +0100

    fixed stop

--- runtime_desktop.go
+++ runtime_desktop.go
@@ -17,7 +17,14 @@ import (
 	ui "github.com/thommil/tge/ui"
 )
 
+// init ensure that we're running on main thread
+func init() {
+	runtime.LockOSThread()
+}
+
+// -------------------------------------------------------------------- //
 // Runtime implementation
+// -------------------------------------------------------------------- //
 type desktopRuntime struct {
 	app      App
 	window   *glfw.Window
@@ -25,6 +32,8 @@ type desktopRuntime struct {
 	ui       ui.UI
 	player   player.Player
 	physics  physics.Physics
+	ticker   *time.Ticker
+	isPaused bool
 }
 
 func (runtime desktopRuntime) GetRenderer() renderer.Renderer {
@@ -44,21 +53,21 @@ func (runtime desktopRuntime) GetPhysics() physics.Physics {
 }
 
 func (runtime desktopRuntime) Stop() {
+	runtime.ticker.Stop()
 	runtime.app.OnPause()
 	runtime.app.OnStop()
 	runtime.window.SetShouldClose(true)
 }
 
-// init ensure that we're running on main thread
-func init() {
-	runtime.LockOSThread()
-}
-
+// -------------------------------------------------------------------- //
 // Main
+// -------------------------------------------------------------------- //
 func doRun(app App, settings *Settings) error {
 	log.Println("doRun()")
 
+	// -------------------------------------------------------------------- //
 	// Init
+	// -------------------------------------------------------------------- //
 	err := glfw.Init()
 	if err != nil {
 		return err
@@ -84,72 +93,83 @@ func doRun(app App, settings *Settings) error {
 		return err
 	}
 
-	// Window callbacks
-	var resizeAtStart sync.Once
-	var paused = true
-	window.SetSizeCallback(func(w *glfw.Window, width int, height int) {
-		// Windows minify issue
-		if !paused && width > 0 {
-			app.OnResize(width, height)
-		}
-	})
-
-	window.SetCloseCallback(func(w *glfw.Window) {
-		if !paused {
-			app.OnPause()
-		}
-		app.OnStop()
-	})
-
-	window.SetFocusCallback(func(w *glfw.Window, focused bool) {
-		if focused && paused {
-			paused = false
-			app.OnResume()
-			resizeAtStart.Do(func() {
-				if runtime.GOOS != "windows" {
-					app.OnResize(settings.Width, settings.Height)
-				}
-			})
-		} else if !paused {
-			paused = true
-			app.OnPause()
-		}
-	})
-
-	// Start
+	// Start GLFW
 	window.MakeContextCurrent()
 
 	// Instanciate Runtime
 	desktopRuntime := desktopRuntime{
-		app:    app,
-		window: window,
+		app:      app,
+		window:   window,
+		isPaused: true,
 	}
+
+	// Start App
 	app.OnStart(&desktopRuntime)
 
-	// OS Specific
+	// OS Specific - Windows do not focus at start
 	if runtime.GOOS == "windows" {
 		app.OnResume()
-		paused = false
+		desktopRuntime.isPaused = false
 		app.OnResize(settings.Width, settings.Height)
 	}
 
+	// -------------------------------------------------------------------- //
 	// Ticker Loop
+	// -------------------------------------------------------------------- //
 	tpsDelay := time.Duration(1000000000 / settings.Physics.TPS)
-	ticker := time.NewTicker(tpsDelay)
-	defer ticker.Stop()
+	desktopRuntime.ticker = time.NewTicker(tpsDelay)
+	defer desktopRuntime.ticker.Stop()
 	go func() {
-		for range ticker.C {
-			if !paused {
+		for range desktopRuntime.ticker.C {
+			if !desktopRuntime.isPaused {
 				app.OnTick(tpsDelay)
 			}
 		}
 	}()
 
+	// -------------------------------------------------------------------- //
+	// Callbacks
+	// -------------------------------------------------------------------- //
+	var resizeAtStart sync.Once
+
+	window.SetSizeCallback(func(w *glfw.Window, width int, height int) {
+		// OS Specific - Windows call resize to 0
+		if !desktopRuntime.isPaused && width > 0 {
+			app.OnResize(width, height)
+		}
+	})
+
+	window.SetFocusCallback(func(w *glfw.Window, focused bool) {
+		if focused && desktopRuntime.isPaused {
+			desktopRuntime.isPaused = false
+			app.OnResume()
+			// OS Specific - MacOS do not resize at start
+			resizeAtStart.Do(func() {
+				if runtime.GOOS != "windows" {
+					app.OnResize(settings.Width, settings.Height)
+				}
+			})
+		} else if !desktopRuntime.isPaused {
+			desktopRuntime.isPaused = true
+			app.OnPause()
+		}
+	})
+
+	window.SetCloseCallback(func(w *glfw.Window) {
+		desktopRuntime.ticker.Stop()
+		if !desktopRuntime.isPaused {
+			app.OnPause()
+		}
+		app.OnStop()
+	})
+
+	// -------------------------------------------------------------------- //
 	// Render Loop
+	// -------------------------------------------------------------------- //
 	fpsDelay := time.Duration(1000000000 / settings.Renderer.FPS)
 	var elapsedFpsTime time.Duration
 	for !window.ShouldClose() {
-		if !paused {
+		if !desktopRuntime.isPaused {
 			startFps := time.Now()
 			app.OnRender(elapsedFpsTime)
 			window.SwapBuffers()
